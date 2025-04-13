255a048bf62919290e599912e1084d31041b5a00
Arash Shahsavari <sharash@student.chalmers.se>
2025-04-13 13:53:05 +0200

    Fix indent bug

--- sr.py
+++ sr.py
@@ -104,77 +104,77 @@ if __name__ == "__main__":
                     if wandb_logger:
                         wandb_logger.log_metrics(logs)
 
-            if current_step % opt['train']['val_freq'] == 0:
-                avg_psnr = 0.0
-                avg_loss = 0.0  # Add this line
-                idx = 0
-                result_path = '{}/{}'.format(opt['path']['results'], current_epoch)
-                os.makedirs(result_path, exist_ok=True)
-
-                diffusion.set_new_noise_schedule(
-                    opt['model']['beta_schedule']['val'], schedule_phase='val')
-                
-                with torch.no_grad():  # Add this context manager
-                    for _, val_data in enumerate(val_loader):
-                        idx += 1
-                        diffusion.feed_data(val_data)
-                        
-                        # Get validation loss - modify your test() method to return loss
-                        val_loss = diffusion.test(
-                            continous=False, 
-                            ddim=opt['model']['ddim_sampling'], 
-                            timesteps=opt['model']['ddim_timesteps']
-                        )
-                        
-                        visuals = diffusion.get_current_visuals()
-                        sr_img = Metrics.tensor2img(visuals['SR'])
-                        hr_img = Metrics.tensor2img(visuals['HR'])
-                        lr_img = Metrics.tensor2img(visuals['LR'])  # uint8
-                        fake_img = Metrics.tensor2img(visuals['INF'])  # uint8
-
-                        # generation
-                        Metrics.save_img(
-                            hr_img, '{}/{}_{}_hr.png'.format(result_path, current_step, idx))
-                        Metrics.save_img(
-                            sr_img, '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
-                        Metrics.save_img(
-                            lr_img, '{}/{}_{}_lr.png'.format(result_path, current_step, idx))
-                        Metrics.save_img(
-                            fake_img, '{}/{}_{}_inf.png'.format(result_path, current_step, idx))
-                        tb_logger.add_image(
-                            'Iter_{}'.format(current_step),
-                            np.transpose(np.concatenate(
-                                (fake_img, sr_img, hr_img), axis=1), [2, 0, 1]),
-                            idx)
-                        avg_psnr += Metrics.calculate_psnr(sr_img, hr_img)
-                        avg_loss += val_loss.item()  # Accumulate validation loss
-
-                        if wandb_logger:
-                            wandb_logger.log_image(
-                                f'validation_{idx}', 
-                                np.concatenate((fake_img, sr_img, hr_img), axis=1)
-                            )
-
-                    avg_psnr = avg_psnr / idx
-                    avg_loss = avg_loss / idx
+                if current_step % opt['train']['val_freq'] == 0:
+                    avg_psnr = 0.0
+                    avg_loss = 0.0  # Add this line
+                    idx = 0
+                    result_path = '{}/{}'.format(opt['path']['results'], current_epoch)
+                    os.makedirs(result_path, exist_ok=True)
 
                     diffusion.set_new_noise_schedule(
-                        opt['model']['beta_schedule']['train'], schedule_phase='train')
-                    # log
-                    logger.info('# Validation # PSNR: {:.4e}, Loss: {:.4e}'.format(avg_psnr, avg_loss))
-                    logger_val = logging.getLogger('val')  # validation logger
-                    logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}'.format(
-                        current_epoch, current_step, avg_psnr))
-                    # tensorboard logger
-                    tb_logger.add_scalar('psnr', avg_psnr, current_step)
+                        opt['model']['beta_schedule']['val'], schedule_phase='val')
+                    
+                    with torch.no_grad():  # Add this context manager
+                        for _, val_data in enumerate(val_loader):
+                            idx += 1
+                            diffusion.feed_data(val_data)
+                            
+                            # Get validation loss - modify your test() method to return loss
+                            val_loss = diffusion.test(
+                                continous=False, 
+                                ddim=opt['model']['ddim_sampling'], 
+                                timesteps=opt['model']['ddim_timesteps']
+                            )
+                            
+                            visuals = diffusion.get_current_visuals()
+                            sr_img = Metrics.tensor2img(visuals['SR'])
+                            hr_img = Metrics.tensor2img(visuals['HR'])
+                            lr_img = Metrics.tensor2img(visuals['LR'])  # uint8
+                            fake_img = Metrics.tensor2img(visuals['INF'])  # uint8
 
-                    if wandb_logger:
-                        wandb_logger.log_metrics({
-                            'validation/val_psnr': avg_psnr,
-                            'validation/val_loss': avg_loss,
-                            'training_iteration': current_step  # Link to training progress
-                        })
-                        val_step += 1
+                            # generation
+                            Metrics.save_img(
+                                hr_img, '{}/{}_{}_hr.png'.format(result_path, current_step, idx))
+                            Metrics.save_img(
+                                sr_img, '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
+                            Metrics.save_img(
+                                lr_img, '{}/{}_{}_lr.png'.format(result_path, current_step, idx))
+                            Metrics.save_img(
+                                fake_img, '{}/{}_{}_inf.png'.format(result_path, current_step, idx))
+                            tb_logger.add_image(
+                                'Iter_{}'.format(current_step),
+                                np.transpose(np.concatenate(
+                                    (fake_img, sr_img, hr_img), axis=1), [2, 0, 1]),
+                                idx)
+                            avg_psnr += Metrics.calculate_psnr(sr_img, hr_img)
+                            avg_loss += val_loss.item()  # Accumulate validation loss
+
+                            if wandb_logger:
+                                wandb_logger.log_image(
+                                    f'validation_{idx}', 
+                                    np.concatenate((fake_img, sr_img, hr_img), axis=1)
+                                )
+
+                        avg_psnr = avg_psnr / idx
+                        avg_loss = avg_loss / idx
+
+                        diffusion.set_new_noise_schedule(
+                            opt['model']['beta_schedule']['train'], schedule_phase='train')
+                        # log
+                        logger.info('# Validation # PSNR: {:.4e}, Loss: {:.4e}'.format(avg_psnr, avg_loss))
+                        logger_val = logging.getLogger('val')  # validation logger
+                        logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}'.format(
+                            current_epoch, current_step, avg_psnr))
+                        # tensorboard logger
+                        tb_logger.add_scalar('psnr', avg_psnr, current_step)
+
+                        if wandb_logger:
+                            wandb_logger.log_metrics({
+                                'validation/val_psnr': avg_psnr,
+                                'validation/val_loss': avg_loss,
+                                'training_iteration': current_step  # Link to training progress
+                            })
+                            val_step += 1
 
                 if current_step % opt['train']['save_checkpoint_freq'] == 0:
                     logger.info('Saving models and training states.')
