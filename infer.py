c19bbccc738abf466a0b11933b09188c044266c3
Arash Shahsavari <sharash@student.chalmers.se>
2025-04-19 12:15:48 +0200

    Retain filename in inference output

--- infer.py
+++ infer.py
@@ -73,29 +73,41 @@ if __name__ == "__main__":
         diffusion.test(continous=True, ddim=opt['model']['ddim_sampling'], timesteps=opt['model']['ddim_timesteps'])
         visuals = diffusion.get_current_visuals(need_LR=False)
 
+        # Get the original filename from the dataset
+        original_filename = os.path.splitext(os.path.basename(val_set.hr_path[val_data['Index'][0]]))[0]
+        # Example: Extracts "0-0-0_0009_0001" from "0-0-0_0009_0001.png"
+
         hr_img = Metrics.tensor2img(visuals['HR'])  # uint8
         fake_img = Metrics.tensor2img(visuals['INF'])  # uint8
 
         sr_img_mode = 'grid'
         if sr_img_mode == 'single':
-            # single img series
             sr_img = visuals['SR']  # uint8
             sample_num = sr_img.shape[0]
             for iter in range(0, sample_num):
                 Metrics.save_img(
-                    Metrics.tensor2img(sr_img[iter]), '{}/{}_{}_sr_{}.png'.format(result_path, current_step, idx, iter))
+                    Metrics.tensor2img(sr_img[iter]), 
+                    f'{result_path}/{original_filename}_sr_{iter}.png'  # New naming
+                )
         else:
-            # grid img
             sr_img = Metrics.tensor2img(visuals['SR'])  # uint8
             Metrics.save_img(
-                sr_img, '{}/{}_{}_sr_process.png'.format(result_path, current_step, idx))
+                sr_img, 
+                f'{result_path}/{original_filename}_sr_process.png'  # New naming
+            )
             Metrics.save_img(
-                Metrics.tensor2img(visuals['SR'][-1]), '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
+                Metrics.tensor2img(visuals['SR'][-1]), 
+                f'{result_path}/{original_filename}_sr.png'  # New naming
+            )
 
         Metrics.save_img(
-            hr_img, '{}/{}_{}_hr.png'.format(result_path, current_step, idx))
+            hr_img, 
+            f'{result_path}/{original_filename}_hr.png'  # New naming
+        )
         Metrics.save_img(
-            fake_img, '{}/{}_{}_inf.png'.format(result_path, current_step, idx))
+            fake_img, 
+            f'{result_path}/{original_filename}_inf.png'  # New naming
+        )
 
         if wandb_logger and opt['log_infer']:
             wandb_logger.log_eval_data(fake_img, Metrics.tensor2img(visuals['SR'][-1]), hr_img)
